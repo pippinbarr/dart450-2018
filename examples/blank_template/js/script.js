a061e1176ad6f4485f6fa1b06fa525fe0296d533
Pippin Barr <pippin.barr@gmail.com>
2018-01-16 08:25:48 -0500

    Added blank template for exercises and projects

---
+++ examples/blank_template/js/script.js
@@ -0,0 +1,15 @@
+/**********************************************
+
+DART 450, Winter 2018
+Title of Project
+Author
+
+Description of what the script does...
+
+**********************************************/
+
+$(document).ready() {
+
+  // Insert jQuery code here to run when the page is loaded
+  
+}
